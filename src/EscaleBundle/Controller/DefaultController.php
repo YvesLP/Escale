121bc1046ea70fc7a904a33afe161686097f6ccb
Florian Grandjean <florian.pdf@gmail.com>
2016-07-10 12:38:24 +0200

    vue 4

--- src/EscaleBundle/Controller/DefaultController.php
+++ src/EscaleBundle/Controller/DefaultController.php
@@ -20,4 +20,19 @@ class DefaultController extends Controller
             'spots' => $spots
         ));
     }
+    
+    public function showAction(Request $request)
+    {
+        $location = str_replace('%20', ' ', $request->query->get('location'));
+
+        $em = $this->getDoctrine()->getManager();
+        $services = $em->getRepository('EscaleBundle:Service')->findAll();
+        $sports = $em->getRepository('EscaleBundle:Sport')->findAll();
+        
+        return $this->render('EscaleBundle:Default:map_show.html.twig', array(
+            'location' => $location,
+            'services' => $services,
+            'sports' => $sports
+        ));
+    }
 }
